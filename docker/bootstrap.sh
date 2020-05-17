1c3f17429998cdc512c2e6cbcc25a437e0d2a781
Vital Ozierski <vital@ozerich.com>
2020-05-17 19:42:19 +0300

    composer

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-php /var/www/app/artisan key:generate
-php /var/www/app/artisan passport:install
-php /var/www/app/artisan migrate --force
-
+cd /var/www/app
+php artisan key:generate
+php artisan passport:install
+php artisan migrate --force
