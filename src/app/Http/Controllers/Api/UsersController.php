5ebba16ecec99088ef6fc9a16429968a11fe2050
gennadijbakunovic <ionenice@yandedx.ru>
2020-05-17 21:59:48 +0300

    Fix

--- src/app/Http/Controllers/Api/UsersController.php
+++ src/app/Http/Controllers/Api/UsersController.php
@@ -5,14 +5,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\user\UsersApiResource;
-use App\Jobs\Queues\TestQueueJob;
 use App\Repositories\Eloquent\AdministratorRepository;
 
 class UsersController extends Controller
 {
     public function index(AdministratorRepository $repository){
-
-        dispatch(new TestQueueJob())->delay(5);
         return UsersApiResource::collection($repository->all());
     }
 
