c171d4c8db1ad5c27a356346db65cb3d8feb7361
Vital Ozierski <v.ozierski@ozitag.com>
2020-07-19 12:50:27 +0300

    add artisan clear commands to bootstrap files

--- docker/bootstrap.production.sh
+++ docker/bootstrap.production.sh
@@ -4,9 +4,14 @@ cd /var/www/app/
 
 chmod 777 -R storage
 
+php artisan config:clear
+
 php artisan migrate --force
 php artisan tager:seo-flush
 php artisan tager:mail-flush
 php artisan tager:settings-flush
 php artisan tager:menus-flush
 php artisan tager:banners-flush
+
+php artisan cache:clear
+php artisan optimize
--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -6,9 +6,14 @@ chmod 777 -R storage
 
 composer i --ignore-platform-reqs
 
+php artisan config:clear
+
 php artisan migrate --force
 php artisan tager:seo-flush
 php artisan tager:mail-flush
 php artisan tager:settings-flush
 php artisan tager:menus-flush
 php artisan tager:banners-flush
+
+php artisan cache:clear
+php artisan optimize
