ce9b8ed8713b8bf8a1cdc9e2854554ca8aed28a1
Vital Ozierski <v.ozierski@ozitag.com>
2020-08-17 13:36:07 +0300

    add

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -2,10 +2,7 @@
 
 namespace App\Http;
 
-use OZiTAG\Tager\Backend\Admin\Http\Kernel as HttpKernel;
-
-// For apps without admin:
-// use OZiTAG\Tager\Backend\Core\Http\Kernel as HttpKernel;
+use OZiTAG\Tager\Backend\Core\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
