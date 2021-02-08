6063c797988a7cf7ddff1dca7c3f67a8554388e9
Vital Ozierski <v.ozierski@ozitag.com>
2021-02-08 20:10:40 +0300

    update routes config

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -1,9 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::post('/upload', [OZiTAG\Tager\Backend\Files\TagerBackendFilesController::class, 'upload']);
-
-Route::group(['namespace' => 'App\Http\Controllers'], function () {
-
-});
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'App\Http\Controllers', 'middleware' => 'api.cache'], function () {
+Route::group(['middleware' => 'api.cache'], function () {
 
 });
