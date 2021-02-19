bae2fa0d119e9345d4eef09f914f0ebecea48b53
Vital Ozierski <v.ozierski@ozitag.com>
2021-02-19 14:45:59 +0300

    add SEO service provider to default

--- src/config/app.php
+++ src/config/app.php
@@ -187,10 +187,11 @@ return [
 
         OZiTAG\Tager\Backend\Cron\CronServiceProvider::class,
         OZiTAG\Tager\Backend\Mail\MailServiceProvider::class,
-        OZiTAG\Tager\Backend\Settings\SettingsServiceProvider::class,
         OZiTAG\Tager\Backend\Menus\MenusServiceProvider::class,
         OZiTAG\Tager\Backend\Pages\PagesServiceProvider::class,
-        OZiTAG\Tager\Backend\Sitemap\SitemapServiceProvider::class,
+        OZiTAG\Tager\Backend\Settings\SettingsServiceProvider::class,
+        OZiTAG\Tager\Backend\Seo\SeoServiceProvider::class,
+        OZiTAG\Tager\Backend\Sitemap\SitemapServiceProvider::class
     ],
 
     /*
