8583fa34a5db7cddd0d5ef0897b451cc6402d1a9
Vital Ozierski <vital@ozerich.com>
2024-06-20 11:35:44 +0200

    Passport Laravel 11 Support

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -4,7 +4,10 @@ cd /var/www/app || exit
 
 rm -rf bootstrap/cache/*
 php artisan optimize:clear
+
+php artisan vendor:publish --tag=passport-migrations || false
 php artisan migrate --force
+
 php artisan tager:mail-flush
 php artisan storage:link --force
 
